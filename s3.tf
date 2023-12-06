4833f892e0976455dd5a901126ccc4d70b606307
Daniel Kirkwood <daniel.g.kirkwood@gmail.com>
2023-12-06 14:56:44 +1100

    initial commit

---
+++ s3.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "backups" {
+  bucket = "dk-mongo-backups-dec"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.backups.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.backups.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
+resource "aws_s3_bucket_acl" "public-read" {
+  depends_on = [     
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example, ]
+  bucket = aws_s3_bucket.backups.id
+  acl = "public-read"
+}
